7c29b527e115080e525c24e340de195c85b94ad4
tokada <theodoreokada@gmail.com>
2019-11-22 14:14:40 +0900

    102 with clock

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
-var time = new Date().getHours();
-var messageText;
 var noon = 12;
 var evening = 18; // 6PM
 var wakeupTime = 9; // 9AM
 var lunchTime = 12; // 12PM
 var partyTime = 17; // 5PM
 var napTime = lunchTime + 2; // 2PM
+var time = new Date().getHours();
 
-var message = document.getElementById("timeEvent");
+var updateClock = function ()
+{
+	var lolcat = document.getElementById("lolcat");
+	var messageText;
+	var message = document.getElementById("timeEvent");
+	var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/wakeUpTime.jpg";
 
-if (time == partyTime){
+	if (time == partyTime){
     messageText = "IZ PARTEE TIME!!";
-} else if (time == napTime) {
+	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
+	} else if (time == napTime) {
     messageText = "IZ NAP TIME...";
-} else if (time == lunchTime) {
+	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+	} else if (time == lunchTime) {
     messageText = "IZ NOM NOM NOM TIME!!";
-} else if (time == wakeupTime) {
-    messageText = "IZ TIME TO GETTUP.";
-} else if (time < noon) {
+	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+	} else if (time == wakeupTime) {
+	messageText = "IZ TIME TO GETTUP.";
+	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+	} else if (time < noon) {
     messageText = "Good morning!";
-} else if (time > evening) {
+	} else if (time > evening) {
     messageText = "Good Evening!";
-} else {
+	} else {
     messageText = "Good afternoon!";
-}
+	}
 
-message.innerText = messageText;
+	message.innerText = messageText;
+	lolcat.src = image;
+
+	showCurrentTime();
+};
+
+var showCurrentTime = function()
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+    // Set hours
+    if (hours >= noon)
+    {
+        meridian = "PM";
+    }
+    if (hours > noon)
+    {
+        hours = hours - 12;
+    }
+    // Set Minutes
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+    // Set Seconds
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+    // put together the string that displays the time
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+    clock.innerText = clockTime;
+};
+
+updateClock();
+
+var oneSecond = 1000;
